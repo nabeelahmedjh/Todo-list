f5642b25e8feec7982f4809daf1cc1b0711b3905
nabeelahmedjh <abdulnabeel40@gmail.com>
2022-08-08 21:00:03 +0500

    Added index.html

--- app.js
+++ app.js
@@ -3,3 +3,13 @@ const bodyParser = require("body-parser");
 
 
 const app = express();
+
+
+app.get("/", (req, res) => {
+    res.send("<h1>TODO list</h1>")
+})
+
+
+app.listen(3000, () => {
+    console.log("App is running at port 3000");
+})
