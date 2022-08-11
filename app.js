9dfb058c4afb28a0c9316164ad6b5e0c97e59cb5
nabeelahmedjh <abdulnabeel40@gmail.com>
2022-08-11 21:51:43 +0500

    completed the version 1 of my todo List

--- app.js
+++ app.js
@@ -1,15 +1,58 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
+// using our own module into app.js
+const date = require(`${__dirname}/date.js`);
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
 
+// it is making server know that load these files 
+app.use(express.static("public"))
+
+
+// creating tasks array
+let dailyTaskList = [];
+let workList = [];
 
 app.get("/", (req, res) => {
-    res.send("<h1>TODO list</h1>")
+
+    const day = date.getDate();
+
+    res.render("list", {
+        list : day,
+        taskList: dailyTaskList
+    })
 })
 
+
+app.post("/", (req, res) => {
+
+    let task = req.body.newTask;
+
+    if (req.body.list === "Work") {
+        workList.push(task);
+        res.redirect("/work")
+    } else {
+        dailyTaskList.push(task);
+        res.redirect("/");
+    }
+})
+
+app.get("/work", (req, res) => {
+
+    res.render("list", {list : "Work", taskList: workList})
+})
+
+
+app.get("/about", (req, res) => {
+    res.render("about")
+})
 
 app.listen(3000, () => {
     console.log("App is running at port 3000");
 })
+
+
+
---
+++ date.js
@@ -0,0 +1,8 @@
+
+exports.getDate = () => {
+    const today = new Date();
+    
+    const options = { weekday: 'long', month : "long",  day: 'numeric' };
+    
+    return today.toLocaleDateString("en-US", options);
+}
